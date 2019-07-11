fb9f177829cfbb7835118f513b8027f4eaf2f5cc
okey <okey.obia@gmail.com>
2019-07-11 12:12:05 -0400

    add side-nav and nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
-import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoListComponent } from "./components/photo-list/photo-list.component";
+import { SidePanelComponent } from "./components/side-panel/side-panel.component";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PhotoListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PhotoListComponent,
+    SidePanelComponent,
+    NavBarComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
